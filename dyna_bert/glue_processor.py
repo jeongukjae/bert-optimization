ca7231e3ce580eb1b5c508936f42c9dc9f3ac4e9
Ukjae Jeong <jeongukjae@gmail.com>
2020-04-19 18:00:52 +0900

    update glue processor

--- dyna_bert/glue_processor.py
+++ dyna_bert/glue_processor.py
@@ -3,6 +3,9 @@ import os
 from abc import ABC, abstractmethod, abstractstaticmethod
 from typing import List, Optional, Tuple
 
+import tensorflow as tf
+import tensorflow_addons as tfa
+
 
 def read_table(input_path: str, delimiter: str = "\t") -> List[List[str]]:
     """
@@ -32,8 +35,31 @@ class GLUEClassificationProcessor(ABC):
     def get_label_to_index(self):
         raise NotImplementedError
 
+    @abstractmethod
+    def update_state(self, target, preds, validation=False):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_metrics(self, validation=False):
+        raise NotImplementedError
+
+    @abstractmethod
+    def reset_states(self, validation=False):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_hash(self):
+        raise NotImplementedError
+
 
 class CoLAProcessor(GLUEClassificationProcessor):
+    def __init__(self):
+        self.mcc = tfa.metrics.MatthewsCorrelationCoefficient(num_classes=1)
+        self.acc = tf.keras.metrics.SparseCategoricalAccuracy()
+
+        self.val_mcc = tfa.metrics.MatthewsCorrelationCoefficient(num_classes=1)
+        self.val_acc = tf.keras.metrics.SparseCategoricalAccuracy()
+
     def get_train(self, path: str):
         return self.parse_cola_dataset(read_table(os.path.join(path, "train.tsv")), False)
 
@@ -45,6 +71,31 @@ class CoLAProcessor(GLUEClassificationProcessor):
 
     def get_label_to_index(self):
         return {"1": 1, "0": 0}
+
+    @tf.function
+    def update_state(self, targets, preds, validation=False):
+        if validation:
+            self.val_acc.update_state(targets, preds)
+            self.val_mcc.update_state(tf.expand_dims(targets, 1), tf.expand_dims(tf.argmax(preds, -1), 1))
+        else:
+            self.acc.update_state(targets, preds)
+            self.mcc.update_state(tf.expand_dims(targets, 1), tf.expand_dims(tf.argmax(preds, -1), 1))
+
+    def get_metrics(self, validation=False):
+        if validation:
+            return {"Acc": self.val_acc.result(), "MCC": self.val_mcc.result()[0]}
+        return {"Acc": self.acc.result(), "MCC": self.mcc.result()[0]}
+
+    def reset_states(self, validation=False):
+        if validation:
+            self.val_acc.reset_states()
+            self.val_mcc.reset_states()
+        else:
+            self.acc.reset_states()
+            self.mcc.reset_states()
+
+    def get_hash(self):
+        return f"{self.val_mcc.result()[0]:.4f}-{self.val_acc.result():.4f}"
 
     @staticmethod
     def parse_cola_dataset(lines: List[List[str]], is_test: bool) -> Tuple[Optional[List[str]], List[str]]:
